b666accfb35696d1eb2cebfed8e169804ab20633
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-18 16:04:53 -0300

    Feature: :tada: First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            
+            <h1>Secret Word (Palavra Secreta)</h1>
+        </div>
+    );
+}
+
+export default App;
