b9542d8f1c612fc0d162633af14102007a9715a0
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-20 14:36:30 -0300

    Feature: :sparkles: Finishing jsx game component

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -3,9 +3,36 @@
 const GameScreen = ({ verifyLetter }) => {
 
 	return (
-		<div>
-			<h1>Game</h1>
-			<button onClick={verifyLetter}>Fim de jogo</button>
+		<div className="game">
+			<p className="points">
+				<span>Pontuação: 000</span>
+			</p>
+			<h1>Secret Word</h1>
+			<h2 className="tip">
+				Adivinhe a palavra <br />
+				<br />
+				Dica: .......
+			</h2>
+
+			<div className="wordContainer">
+				<span className="letter">A</span>
+				<span className="blankSquare"></span>
+			</div>
+
+			<div className="letterContainer">
+				<p>Tente adivinhar uma letra da palavra:</p>
+
+				<form>
+					<input type="text" maxLength="1" required />
+					<button className="btn-primary">Jogar</button>
+				</form>
+			</div>
+
+			<div className="wrongLettersContainer">
+				<p>Letras já utilizadas: </p>
+				<span>a,</span>
+				<span>b,</span>
+			</div>
 		</div>
 	)
 }
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,12 +1,14 @@
-import styles from './StartScreen.module.css'
+// import styles from './StartScreen.module.css'
 
 const StartScreen = ({ startGame }) => {
 
 	return (
-		<div className={styles.start}>
+		<div>
 			<h1>Secret Word</h1>
 			<h2>Clique no botão abaixo para começar a jogar</h2>
-			<button onClick={startGame}>COMEÇAR O JOGO</button>
+			<button onClick={startGame} className="btn-primary">
+				Começar o jogo
+			</button>
 		</div>
 	)
 }
