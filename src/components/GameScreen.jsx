f16111fb5a94548a48218b9a673f0ba38a9c8bc8
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-20 16:03:48 -0300

    Feature: :sparkles: Finishing principal css for gameScreen

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,37 +1,36 @@
-// import styles from './GameScreen.module.css'
+import styles from './GameScreen.module.css'
 
 const GameScreen = ({ verifyLetter }) => {
 
 	return (
-		<div className="game">
-			<p className="points">
+		<div className={styles.game}>
+			<p className={styles.points}>
 				<span>Pontuação: 000</span>
 			</p>
-			<h1>Secret Word</h1>
-			<h2 className="tip">
-				Adivinhe a palavra <br />
-				<br />
-				Dica: .......
+			<h1>Adivinhe a palavra</h1>
+			<h2 className={styles.tip}>
+				Dica sobre a palavra: 
+				<span> Dica </span>
 			</h2>
 
-			<div className="wordContainer">
-				<span className="letter">A</span>
-				<span className="blankSquare"></span>
+			<div className={styles.wordContainer}>
+				<span className={styles.letter}>A</span>
+				<span className={styles.blankSquare}></span>
 			</div>
 
-			<div className="letterContainer">
+			<div className={styles.letterContainer}>
 				<p>Tente adivinhar uma letra da palavra:</p>
 
 				<form>
 					<input type="text" maxLength="1" required />
-					<button className="btn-primary">Jogar</button>
+					<button className="btn-primary">Jogar!</button>
 				</form>
 			</div>
 
-			<div className="wrongLettersContainer">
+			<div className={styles.wrongLettersContainer}>
 				<p>Letras já utilizadas: </p>
-				<span>a,</span>
-				<span>b,</span>
+				<span>a, </span>
+				<span>b, </span>
 			</div>
 		</div>
 	)
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,13 +1,17 @@
-// import styles from './StartScreen.module.css'
+import styles from './StartScreen.module.css'
 
 const StartScreen = ({ startGame }) => {
 
 	return (
-		<div>
+		<div className={styles.startContainer}>
 			<h1>Secret Word</h1>
 			<h2>Clique no botão abaixo para começar a jogar</h2>
-			<button onClick={startGame} className="btn-primary">
-				Começar o jogo
+			<button onClick={startGame} className={`${styles.playBtn} btn-primary`}>
+				<img 
+					src="/play.svg" 
+					alt="Botão de jogar"
+					height="22" 
+				/> Play
 			</button>
 		</div>
 	)
